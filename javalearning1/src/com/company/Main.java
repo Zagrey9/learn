dc7c15c3e00a6f85639af7032e8238226bfa101a
Oleg <91551846+Zagrey9@users.noreply.github.com>
2021-11-24 04:23:27 +0400

    Update Main.java

--- javalearning1/src/com/company/Main.java
+++ javalearning1/src/com/company/Main.java
@@ -1,24 +1,17 @@
 package com.company;
 
-public class Main {
+class Main {
 
-    public static boolean isSquare(int[] array) {
-        for (int i : array) {
-            System.out.println(isSquare(i) ? i + " Полный квадрат" : i + " Неполный квадрат");
+
+
+        public static void main (String[]args){
+            double x = 10;
+            double a = Math.toRadians(x);
+            double sin=  Math.sin(a);
+            double cos = Math.cos(a);
+            System.out.println(Math.cos(a)+Math.sin(a));
+
+
+
         }
-        return false;
     }
-
-    public static boolean isSquare(int x) {
-        int n = (int) Math.sqrt(x);
-        int r = n*n;
-        return r == x;
-    }
-
-    public static void main(String[] args) {
-        int[] array = new int[]{4, 8, 9, 16, 18, 26, 36, 40, 100};
-        isSquare(array);
-        if (isSquare(array)) System.out.println("Неверно");
-
-    }
-}
