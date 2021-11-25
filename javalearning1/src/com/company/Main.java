aaeea6b88024c7c49ffeb773d33762029d72634f
Oleg <91551846+Zagrey9@users.noreply.github.com>
2021-11-25 05:41:33 +0400

    Update Main.java

--- javalearning1/src/com/company/Main.java
+++ javalearning1/src/com/company/Main.java
@@ -1,15 +1,21 @@
 package com.company;
 
 class Main {
+    public static void func (){
 
+        double x = 10;
+        double a = Math.toRadians(x);
+        double sin=  Math.sin(a);
+        double cos = Math.cos(a);
+        System.out.println(Math.cos(a)+Math.sin(a));
+    }
 
 
         public static void main (String[]args){
-            double x = 10;
-            double a = Math.toRadians(x);
-            double sin=  Math.sin(a);
-            double cos = Math.cos(a);
-            System.out.println(Math.cos(a)+Math.sin(a));
+
+
+            for ( int i=1; i<10; i++)
+                System.out.println(Math.cos(i)+Math.sin(i));
 
 
 
